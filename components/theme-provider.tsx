1dc3890e1f7a0cd4125412117d3d2a76ad5391f4
Niruth Ananth <niruthananth03@gmail.com>
2025-01-09 15:56:28 +0530

    .

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,7 +2,15 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { ReactNode, ComponentProps } from "react"
 
-export function ThemeProvider({ children, ...props }) {
+// Infer props from NextThemesProvider
+type ThemeProviderProps = ComponentProps<typeof NextThemesProvider>
+
+interface CustomThemeProviderProps extends ThemeProviderProps {
+  children: ReactNode
+}
+
+export function ThemeProvider({ children, ...props }: CustomThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
